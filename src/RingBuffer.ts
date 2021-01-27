a5dfcca19be54b01bc2484ed72ce7eac20428438
Emil Loer <emil@koffietijd.net>
2021-01-27 19:35:00 +0100

    clean up

--- src/RingBuffer.ts
+++ src/RingBuffer.ts
@@ -60,7 +60,6 @@ export default class RingBuffer {
             return 0;
         }
 
-
         // Compute bytes to write and first/last split
         let bytesWritten;
         if (writePointer >= readPointer) {
